f85062a1a0a140573507f6cae77d06fe0384027f
unknown <ryanyang1414@gmail.com>
2026-09-04 14:02:41 -0700

    commit

--- myscript.js
+++ myscript.js
@@ -5,5 +5,17 @@ $(document).ready(function() {
 	$("h1").click(function() {
 		$("p").addClass("bright");
 		$("img").addClass("border");
-	});
+	
+	
+});
+
+$(".intro").click(function(){
+  $(".disappear").toggle();
+});
+
+$(".imagetoggler").hover(function(){
+  alert("click me, bet you cant lol");	
+
+});
+
 });
